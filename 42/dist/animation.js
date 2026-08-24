377964c87b4646f423afbfc31bb9f6692defcf09
foreverzmy <foreverzmy@users.noreply.github.com>
2026-08-24 23:03:30 +0800

    Deploy animations batch 04

---
+++ 42/dist/animation.js
@@ -0,0 +1,7 @@
+import { CanvasTimelinePlayer } from "../../_shared/canvas-timeline/dist/index.js";
+const canvas = document.querySelector("#animation");
+if (!canvas)
+    throw new Error("Missing #animation canvas");
+const player = await CanvasTimelinePlayer.create({ canvas, lookaheadFrames: 24 });
+globalThis.animation42 = player;
+document.addEventListener("pointerdown", () => player.play(), { once: true });
